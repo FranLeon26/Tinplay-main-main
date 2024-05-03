2774672738d5e2aa3fd32a2ed157b159b7c32bca
FranLeon26 <franleoon26@gmail.com>
2024-05-03 09:54:44 -0300

    Inicio-Responsive

---
+++ index.js
@@ -0,0 +1,134 @@
+function mostrarImagenBaquettinRojo(idImagen) {
+  var imagen = document.getElementById("imagenMostrada");
+
+  if (idImagen === "imagen1") {
+    imagen.src = "../images/imagen1-grande.jpg";
+  } else if (idImagen === "imagen2") {
+    imagen.src = "../images/imagen2-grande.jpg";
+  } else if (idImagen === "imagen3") {
+    imagen.src = "../images/imagen3-grande.jpg";
+  } else if (idImagen === "imagen4") {
+    imagen.src = "../images/image-grande.jpg";
+  }
+}
+function mostrarImagen_baquettin_negro(idImagen) {
+  var imagen = document.getElementById("imagenMostrada_baquettin_negro");
+
+  if (idImagen === "imagen1") {
+    imagen.src = "../images/baquettin-negro_grande1.jpg";
+  } else if (idImagen === "imagen2") {
+    imagen.src = "../images/baquettin-negro_grande2.jpg";
+  } else if (idImagen === "imagen3") {
+    imagen.src = "../images/baquettin-negro_grande3.jpg";
+  } else if (idImagen === "imagen4") {
+    imagen.src = "../images/baquettin-negro_grande4.jpg";
+  }
+}
+function mostrarImagen(idImagen) {
+  var imagen = document.getElementById("imagenMostrada-baquettin_blanco");
+
+  if (idImagen === "imagen1") {
+    imagen.src = "../images/baquettin-blanco-grande1.jpg";
+  } else if (idImagen === "imagen2") {
+    imagen.src = "../images/baquettin-blanco-grande2.jpg";
+  } else if (idImagen === "imagen3") {
+    imagen.src = "../images/baquettin-blanco-grande3.jpg";
+  } else if (idImagen === "imagen4") {
+    imagen.src = "../images/baquettin-blanco-grande4.jpg";
+  } }
+  function mostrarImagenBugattinBlanco(idImagen) {
+    var imagen = document.getElementById("imagenMostrada_Bugattin_Blanco");
+  
+    if (idImagen === "imagen1") {
+      imagen.src = "../images/bugattin-blanco-grande1.jpg";
+    } else if (idImagen === "imagen2") {
+      imagen.src = "../images/bugattin-blanco-grande2.jpg";
+    } else if (idImagen === "imagen3") {
+      imagen.src = "../images/bugattin-blanco-grande3.jpg";
+    } else if (idImagen === "imagen4") {
+      imagen.src = "../images/bugattin-blanco-grande4.jpg";
+    } }
+    function mostrarImagenBugattinNegro(idImagen) {
+      var imagen = document.getElementById("imagenMostrada_Bugattin_negro");
+    
+      if (idImagen === "imagen1") {
+        imagen.src = "../images/bugattin-negro-grande1.jpg";
+      } else if (idImagen === "imagen2") {
+        imagen.src = "../images/bugattin-negro-grande2.jpg";
+      } else if (idImagen === "imagen3") {
+        imagen.src = "../images/bugattin-negro-grande4.jpg";
+      } else if (idImagen === "imagen4") {
+        imagen.src = "../images/bugattin-negro-grande3.jpg";
+      } }
+      function mostrarImagenBugattinRojo(idImagen) {
+        var imagen = document.getElementById("imagenMostrada_Bugattin_Rojo");
+      
+        if (idImagen === "imagen1") {
+          imagen.src = "../images/bugattin-rojo-grande1.jpg";
+        } else if (idImagen === "imagen2") {
+          imagen.src = "../images/bugattin-rojo-grande2.jpg";
+        } else if (idImagen === "imagen3") {
+          imagen.src = "../images/bugattin-rojo-grande1.jpg";
+        } else if (idImagen === "imagen4") {
+          imagen.src = "../images/bugattin-rojo-grande2.jpg";
+        } }
+        function mostrarImagenWagonRojo(idImagen) {
+          var imagen = document.getElementById("imagenMostrada_Wagon_Rojo");
+        
+          if (idImagen === "imagen1") {
+            imagen.src = "../images/wagon-rojo-grande1.jpg";
+          } else if (idImagen === "imagen2") {
+            imagen.src = "../images/wagon-rojo-grande2.jpg";
+          } else if (idImagen === "imagen3") {
+            imagen.src = "../images/wagon-rojo-grande3.jpg";
+          } else if (idImagen === "imagen4") {
+            imagen.src = "../images/wagon-rojo-grande4.jpg";
+          } }
+          function mostrarImagenWagonBlanco(idImagen) {
+            var imagen = document.getElementById("imagenMostrada_Wagon_blanco");
+          
+            if (idImagen === "imagen1") {
+              imagen.src = "../images/wagon-blanco-grande1.jpg";
+            } else if (idImagen === "imagen2") {
+              imagen.src = "../images/wagon-blanco-grande2.jpg";
+            } else if (idImagen === "imagen3") {
+              imagen.src = "../images/wagon-blanco-grande3.jpg";
+            } else if (idImagen === "imagen4") {
+              imagen.src = "../images/wagon-blanco-grande4.jpg";
+            } }
+            function mostrarImagenWagonNegro(idImagen) {
+              var imagen = document.getElementById("imagenMostrada_Wagon_negro");
+            
+              if (idImagen === "imagen1") {
+                imagen.src = "../images/wagon-negro-grande1.jpg";
+              } else if (idImagen === "imagen2") {
+                imagen.src = "../images/wagon-negro-grande2.jpg";
+              } else if (idImagen === "imagen3") {
+                imagen.src = "../images/wagon-negro-grande3.jpg";
+              } else if (idImagen === "imagen4") {
+                imagen.src = "../images/wagon-negro-grande4.jpg";
+              } }
+              function mostrarImagenTriciclo(idImagen) {
+                var imagen = document.getElementById("imagenMostrada_Triciclo");
+              
+                if (idImagen === "imagen1") {
+                  imagen.src = "../images/triciclo-grande1.jpg";
+                } else if (idImagen === "imagen2") {
+                  imagen.src = "../images/triciclo-grande2.jpg";
+                } else if (idImagen === "imagen3") {
+                  imagen.src = "../images/triciclo-grande3.jpg";
+                } else if (idImagen === "imagen4") {
+                  imagen.src = "../images/triciclo-grande4.jpg";
+                } }
+                function mostrarImagenSurtidor(idImagen) {
+                  var imagen = document.getElementById("imagenMostrada_Surtidor");
+                
+                  if (idImagen === "imagen1") {
+                    imagen.src = "../images/surtidor-grande1.jpg";
+                  } else if (idImagen === "imagen2") {
+                    imagen.src = "../images/surtidor-grande2.jpg";
+                  } else if (idImagen === "imagen3") {
+                    imagen.src = "../images/surtidor-grande3.jpg";
+                  } else if (idImagen === "imagen4") {
+                    imagen.src = "../images/surtidor-grande4.jpg";
+                  } }
